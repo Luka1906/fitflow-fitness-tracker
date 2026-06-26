20a11f6b08152206f9fceb4a4020680ae7d45e9e
Luka Matovic <lukamatovic1994@gmail.com>
2026-06-25 23:00:42 -0500

    fix: update workout delete confirmation modal to match the app style

--- src/components/profile-components/profile-cards/workout-card/workout-drawer/WorkoutDeleteConfirm.jsx
+++ src/components/profile-components/profile-cards/workout-card/workout-drawer/WorkoutDeleteConfirm.jsx
@@ -2,27 +2,19 @@ import { IoWarning } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import { useFetcher } from "react-router-dom";
 import { createLocalDate } from "../../../../../utils/createLocalDate";
+import { format } from "date-fns";
 
 import { useEffect } from "react";
 
 const WorkoutDeleteConfirm = ({
-  selectedDate,
   selectedWorkout,
   onClose,
   deleteModalIsOpen,
   onDeleteSuccess
 }) => {
-  console.log(selectedWorkout)
+const selectedDate = selectedWorkout?.logged_at? new Date(selectedWorkout.logged_at) : new Date()
   const deleteFetcher = useFetcher();
-  const formattedDate = createLocalDate(selectedDate).toLocaleDateString(
-    "en-US",
-    {
-      month: "short",
-      day: "2-digit",
-      year: "numeric",
-    },
-  );
-
+ 
   useEffect(() => {
     if (!deleteModalIsOpen) return;
 
@@ -31,32 +23,46 @@ const WorkoutDeleteConfirm = ({
     }
   }, [deleteModalIsOpen, deleteFetcher.state, deleteFetcher.data, onDeleteSuccess]);
 
+    
 
   return (
-    <div className=" bg-bg-dark/90 border brightness-125 border-red-400/20 rounded-2xl  backdrop-blur-2xl flex flex-col items-center gap-4  text-white px-6 py-7 relative">
-      <div className="flex flex-col items-center gap-2">
-        <IoWarning className="text-red-400 bg-red-500/15 border border-red-400/20 w-10 h-10 p-2.5 rounded-full" />
-        <h2 className="font-semibold text-red-300/90">Delete Workout Log</h2>
-      </div>
-      <IoMdClose
-        onClick={onClose}
-        className="absolute right-4 w-8 h-8 transition p-2 bg-white/10 rounded-full text-slate-300 border border-white/10  hover:text-white hover:border-white/5 cursor-pointer"
-      />
+     <div className="flex flex-col gap-5 rounded-2xl border border-red-400/20 bg-bg-dark/90 p-4 text-text-primary-paragraph">
+      <header className="flex justify-between">
+        <div className="flex gap-2">
+          <IoWarning className="h-9 w-9 rounded-md bg-red-500/10 p-2 text-red-400" />
 
-      <div className="px-10 text-center">
-        <p className="text-sm text-slate-300">
-          Are you sure you want to delete your
-          <span className="text-white font-medium"> </span> log from
-          <span className="text-white font-medium"> {formattedDate}</span>?
+          <div>
+            <h2 className="font-semibold text-red-300">Delete workout log</h2>
+            <p className="text-sm text-slate-500">
+              This action cannot be undone
+            </p>
+          </div>
+        </div>
+
+        <IoMdClose
+          onClick={onClose}
+          className="h-9 w-9 cursor-pointer rounded-full border border-white/10 bg-white/5 p-2 text-slate-300 transition hover:border-white/20 hover:bg-white/10 hover:text-white"
+        />
+      </header>
+
+      <main className="rounded-xl border border-white/10 bg-black/10 p-4">
+        <p className="text-sm leading-6 text-slate-300">
+          Are you sure you want to delete your log from{" "}
+          <span className="font-semibold text-white">
+            {format(selectedDate, "MMM dd, yyyy")}
+          </span>
+          ?
         </p>
-      </div>
-      <div className="flex gap-3  w-full font-semibold">
+      </main>
+
+      <footer className="flex gap-3">
         <button
           onClick={onClose}
-          className="flex-1 bg-white/10 hover:bg-white/15 py-2 rounded-lg"
+          className="flex-1 cursor-pointer rounded-lg border border-white/10 bg-white/4 py-3 text-[0.9rem] font-medium transition hover:bg-white/10"
         >
           Cancel
         </button>
+
         <deleteFetcher.Form
           className="flex-1"
           method="DELETE"
@@ -64,12 +70,12 @@ const WorkoutDeleteConfirm = ({
         >
           <button
             type="submit"
-            className=" bg-red-500/80 w-full hover:bg-red-500 py-2 rounded-lg"
+            className="w-full cursor-pointer rounded-lg border border-red-500/70 bg-red-500/90 py-3 text-[0.9rem] font-medium text-white transition hover:bg-red-500"
           >
-            Delete
+            Delete log
           </button>
         </deleteFetcher.Form>
-      </div>
+      </footer>
     </div>
   );
 };
